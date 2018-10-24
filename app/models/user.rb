4a233fb6846072c1c6c7acbc58f238ec60546dc6
Myo T Kyaw <myotkyaw@gmail.com>
2018-10-23 18:29:31 -0700

    Add api_key to user table; Update User model to assign an API before creating a user; Update application controller to authenticate every request

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  # Assign an API key on create
+  before_create do |user|
+    user.api_key = generate_api_key
+  end
+
+  private
+  
+  def generate_api_key
+    SecureRandom.base64.tr('+/=', 'Qrt')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_234753) do
+ActiveRecord::Schema.define(version: 2018_10_24_005558) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_key"
   end
 
 end
